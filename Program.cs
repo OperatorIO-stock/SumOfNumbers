8ea96247115d6fd8ccafbef9fc337df2990386c0
OperatorIO-stock <vova.chigorin@yandex.com>
2024-11-21 14:19:53 +0300

    string up

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Random randomNumber = new Random();
-
         int number = randomNumber.Next(0, 100);
 
         for(int i = 0; i < number; number--)
