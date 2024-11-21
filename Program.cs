88add4778021093af6e8ee562c44393cc3ae2cce
OperatorIO-stock <vova.chigorin@yandex.com>
2024-11-21 14:18:17 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SumOfNumbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random randomNumber = new Random();
+
+        int number = randomNumber.Next(0, 100);
+
+        for(int i = 0; i < number; number--)
+        {
+            if(number % 5 == 0)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
