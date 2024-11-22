cbe21260b71824abdef12a2818db4d6750ded7ed
operatorIO-stock <vova.chigorin@yandex.com>
2024-11-22 12:26:42 +0300

    new version program

--- Program.cs
+++ Program.cs
@@ -4,12 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random randomNumber = new Random();
-        int number = randomNumber.Next(0, 100);
+
+        int maxValue = 100;
+        int minValue = 0;
+
+        int multiplicityChecksNumber1 = 3;
+        int multiplicityChecksNumber2 = 5;
+
+
+        Random random = new Random();
+        int number = random.Next(minValue, maxValue);
 
         for(int i = 0; i < number; number--)
         {
-            if(number % 5 == 0)
+            if(number % multiplicityChecksNumber1 == 0 || number % multiplicityChecksNumber2 == 0)
             {
                 Console.WriteLine(number);
             }
